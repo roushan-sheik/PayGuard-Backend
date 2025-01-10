601d902822ba64344e35aecabe69e667db2636db
roushan-sheik <roushansheik@gmail.com>
2025-01-10 15:33:42 +0600

    add: middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
+
+// middlewares for parsing incoming requests
+const corseOptions = {
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+};
+app.use(cors(corseOptions));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("PayGuard Home");
