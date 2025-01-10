68c1978f65e12fc1e13a54bb4eb877e74ed0f7eb
roushan-sheik <roushansheik@gmail.com>
2025-01-10 15:26:24 +0600

    add: basic project setup

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+import { app } from "./app";
+import config from "./config";
+import connectDB from "./db/connectDB";
+
+connectDB()
+  .then(() => {
+    app.listen(config.port, () => {
+      console.log(`\n App is running on port: http://localhost:${config.port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection FAILED!", error.message);
+  });
